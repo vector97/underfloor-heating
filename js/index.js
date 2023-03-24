37dd70dc983fd53aa26d75b27cc0f3818e5a9a8b
Viktor Bogatenkov <vector_97@mail.ru>
2023-03-24 17:05:08 +0400

    feat: complete responsive

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log('scripts');
+new Swiper('.hero__slider', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 10,
+  // autoplay: {
+  //   delay: 3000,
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.hero__slider-btn_next',
+    prevEl: '.hero__slider-btn_prev',
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    560: {
+      spaceBetween: 8,
+    },
+  },
+});
